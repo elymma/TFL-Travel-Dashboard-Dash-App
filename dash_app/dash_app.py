3187dc4c6e9c8d996ca2a84b36c4ce579a42d027
elymma <elymma.mensah.19@ucl.ac.uk>
2022-02-13 00:07:56 +0000

    dropdown and checklists made variables

--- dash_app/dash_app.py
+++ dash_app/dash_app.py
@@ -14,14 +14,14 @@ external_stylesheets = [dbc.themes.LUX]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # ------------------------------------------------------------------------
-# import data and create chart
+# import data and create charts
 
 df = pd.read_excel("cleaned_tfl_dataset_EDIT.xlsx")
 
 fig_line = px.line(df, x="Period ending", y="Journeys (m)", color="Travel Mode",
-                   title="Distribution of TFL Travel Modes")
+                   title="Travel Mode Usage Over Time")
 
-fig_pie = px.pie(df, values="Journeys (m)", names="Travel Mode")
+fig_pie = px.pie(df, values="Journeys (m)", names="Travel Mode", title="Distribution of Travel Modes")
 # ------------------------------------------------------------------------
 # app layout
 
@@ -37,6 +37,42 @@ header = [
     ), width=3),
 ]
 
+dropdown = [
+    dcc.Dropdown(id="select_year",
+                 options=[
+                     {'label': '2010', 'value': 2010},
+                     {'label': '2011', 'value': 2011},
+                     {'label': '2012', 'value': 2012},
+                     {'label': '2013', 'value': 2013},
+                     {'label': '2014', 'value': 2014},
+                     {'label': '2015', 'value': 2015},
+                     {'label': '2016', 'value': 2016},
+                     {'label': '2017', 'value': 2017},
+                     {'label': '2018', 'value': 2018},
+                     {'label': '2019', 'value': 2019},
+                     {'label': '2020', 'value': 2020},
+                     {'label': '2021', 'value': 2021},
+                 ],
+                 style={'width': '40%'}
+                 ),
+]
+
+checklist = [
+    dcc.Checklist(id="select_travel_mode2",
+                  options=[
+                      {'label': 'Bus', 'value': 'Bus'},
+                      {'label': 'Underground', 'value': 'Underground'},
+                      {'label': 'DLR', 'value': 'DLR'},
+                      {'label': 'Tram', 'value': 'Tram'},
+                      {'label': 'Overground', 'value': 'Overground'},
+                      {'label': 'Emirates Airline', 'value': 'Emirates Airline'},
+                      {'label': 'TFL Rail', 'value': 'TFL Rail'},
+                  ],
+
+                  ),
+
+]
+
 app.layout = html.Div(children=[
 
     dbc.Row(
@@ -46,7 +82,6 @@ app.layout = html.Div(children=[
     dbc.Row(
         header,
         justify="between",
-
     ),
 
     dbc.Row(
@@ -56,54 +91,24 @@ app.layout = html.Div(children=[
     dbc.Row(
         html.Div([
             html.H1("TFL TRAVEL DASHBOARD")
-        ], style={"border": "2px black solid"})
-
+        ], style={"border": "1px black solid"})
     ),
 
     dbc.Row(
         html.Br()
     ),
 
-    dbc.Row(
-        dcc.Dropdown(id="select_year",
-                     options=[
-                         {'label': '2010', 'value': 2010},
-                         {'label': '2011', 'value': 2011},
-                         {'label': '2012', 'value': 2012},
-                         {'label': '2013', 'value': 2013},
-                         {'label': '2014', 'value': 2014},
-                         {'label': '2015', 'value': 2015},
-                         {'label': '2016', 'value': 2016},
-                         {'label': '2017', 'value': 2017},
-                         {'label': '2018', 'value': 2018},
-                         {'label': '2019', 'value': 2019},
-                         {'label': '2020', 'value': 2020},
-                         {'label': '2021', 'value': 2021},
-                     ],
-                     style={'width': '40%'}
-                     ),
-
-    ),
-
     dbc.Row([
-        dbc.Col(html.Div([dcc.Graph(id="graph", figure=fig_line)]), width=6),
-        dbc.Col(html.Div([dcc.Graph(id="graph_two", figure=fig_pie)]), width=5)
+        dbc.Col(dropdown),
+        dbc.Col(html.Div([dcc.Graph(id="graph_two", figure=fig_pie)]), width=5),
+        # dbc.Col(checklist)
     ]),
 
-    dbc.Row(
-
-        dcc.Checklist(id="select_travel_mode",
-                      options=[
-                          {'label': 'Bus', 'value': 'Bus'},
-                          {'label': 'Underground', 'value': 'Underground'},
-                          {'label': 'DLR', 'value': 'DLR'},
-                          {'label': 'Tram', 'value': 'Tram'},
-                          {'label': 'Overground', 'value': 'Overground'},
-                          {'label': 'Emirates Airline', 'value': 'Emirates Airline'},
-                          {'label': 'TFL Rail', 'value': 'TFL Rail'},
-                      ],
-
-                      ), ),
+    dbc.Row([
+        # dbc.Col(dropdown),
+        dbc.Col(html.Div([dcc.Graph(id="graph", figure=fig_line)]), width=10),
+        # dbc.Col(checklist)
+    ]),
 
 ])
 
